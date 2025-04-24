4de188ad730027961626aa931066236925a81216
IAGO LEAL DE MATTOS <iago.mattos@aluno.senac.rio>
2025-04-24 18:30:23 -0300

    Adicionando o Desafio

---
+++ codigo/desafio01.py
@@ -0,0 +1,8 @@
+num1 = input("escreva um número: ")
+num2 = input("escreva outro número: ")
+num3 = input("escreva outro número: ")
+
+lista = [num1, num2, num3]
+lista.sort(reverse=False)
+
+print(lista)
